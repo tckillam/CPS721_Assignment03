4aba24800e8d1964d8026f3e3bfb2b8b484cca96
shaghayegh0 <119910382+shaghayegh0@users.noreply.github.com>
2024-10-18 15:40:45 -0400

    Update q2_game.pl
    actually working

--- q2_game.pl
+++ q2_game.pl
@@ -1,3 +1,4 @@
+
 % q2_game.pl
 %%%%% NAME: Shaghayegh Dehghanisanij
 %%%%% NAME: Theresa Killam
@@ -21,38 +22,117 @@ outcome(l). % Loss
 outcome(d). % Draw
 outcome(n). % Did not participate
 
+
+
 % Main solve predicate
 solve([O, P, R, S, T]) :-
-    O = [O1, O2, O3, O4, O5], exactlyOne(n, O),
+
+
+
+    O = [O1, O2, O3, O4, O5], 
     write('Oakville outcomes: '), write(O), nl,  % Debug output for Oakville
-    P = [P1, P2, P3, P4, P5], exactlyOne(n, P),
+    P = [P1, P2, P3, P4, P5], 
     write('Pickering outcomes: '), write(P), nl,  % Debug output for Pickering
-    R = [R1, R2, R3, R4, R5], exactlyOne(n, R),
+    R = [R1, R2, R3, R4, R5], 
     write('Richmond Hill outcomes: '), write(R), nl,  % Debug output for Richmond Hill
-    S = [S1, S2, S3, S4, S5], exactlyOne(n, S),
+    S = [S1, S2, S3, S4, S5], 
     write('Scarborough outcomes: '), write(S), nl,  % Debug output for Scarborough
-    T = [T1, T2, T3, T4, T5], exactlyOne(n, T),
+    T = [T1, T2, T3, T4, T5], 
     write('Toronto outcomes: '), write(T), nl,  % Debug output for Toronto
+
 
     % Check constraint logic
     P1 = l, S1 = w, P2 = w, O2 = l,
     write('Constraint 1 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
 
-    not T1=d, not T1=T2, not T2=d, T3 = n,
+
+    T3 = n, countWins([T1 , T2] , 1) , countLosses([T1 , T2] , 1),
     write('Constraint 2 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
 
-    O4 = n, countWins(O, 2),
+    O4 = n, countWins([O1,O2,O3], 2),
     write('Constraint 3 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
 
     fourExactly(d, [O4, P4, R4, S4, T4]),
     fourExactly(d, [O5, P5, R5, S5, T5]),
     write('Constraint 4 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
 
     countWins([R1, R2, R3], 1), countLosses([R1, R2, R3], 1),
     write('Constraint 5 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
 
     countDraws([O1, O2, O3, P1, P2, P3, R1, R2, R3, S1, S2, S3, T1, T2, T3] , 0),
-    write('Constraint 6 passed'), nl.
+    write('Constraint 6 passed'), nl,
+    write('P:'),write(P), nl,
+    write('S:'),write(S), nl,
+    write('O:'),write(O), nl,
+    write('T:'),write(T), nl,
+    write('R:'),write(R), nl,
+
+    
+    Round1 = [O1 , P1 , R1 , S1 , T1],
+    Round2 = [O2 , P2 , R2 , S2 , T2],
+    Round3 = [O3 , P3 , R3 , S3 , T3],
+    Round4 = [O4 , P4 , R4 , S4 , T4],
+    Round5 = [O5 , P5 , R5 , S5 , T5],
+
+    
+    exactlyOne(n, O),
+    exactlyOne(n, P),
+    exactlyOne(n, S),
+    exactlyOne(n, R),
+    exactlyOne(n, T),
+
+    countLosses(Round1 , X1) , countWins(Round1 , Y1), 
+    countLosses(Round2 , X2) = countWins(Round2 , Y2),
+    countLosses(Round3 , X3) = countWins(Round3 , Y3),
+    countLosses(Round4 , X4) = countWins(Round4 , Y4),
+    countLosses(Round5 , X5) = countWins(Round5 , Y5),
+
+    exactlyOne(n, Round1),
+    exactlyOne(n, Round2),
+    exactlyOne(n, Round3),
+    exactlyOne(n, Round4),
+    exactlyOne(n, Round5).
+
+
+
+cityDomain([C1,C2,C3,C4,C5]) :-
+    outcome(C1),
+    outcome(C2),
+    outcome(C3),
+    outcome(C4),
+    outcome(C5).
+
+
+
+
+
+
+
 
 % Helper predicates to ensure exactly one occurrence of 'n' (did not participate)
 exactlyOne(n, List) :- counter(n, List, 0, 1).
